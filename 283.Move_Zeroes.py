2fc9433446652f7f89d068dd869e8207907cfd44
peter <wzqnls@163.com>
2017-04-08 22:05:19 +0800

    283.Move Zeroes

---
+++ 283.Move_Zeroes.py
@@ -0,0 +1,30 @@
+"""
+Given an array nums, write a function to move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+
+For example, given nums = [0, 1, 0, 3, 12], after calling your function, nums should be [1, 3, 12, 0, 0].
+
+Note:
+
+    You must do this in-place without making a copy of the array.
+    Minimize the total number of operations.
+"""
+
+"""
+解题思路：
+遍历列表
+判断元素是否为0
+为0则移除该元素，并在列表末位添加0
+"""
+
+
+class Solution(object):
+
+    def moveZeroes(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: void Do not return anything, modify nums in-place instead.
+        """
+        for i in nums:
+            if i == 0:
+                nums.remove(i)
+                nums.append(0)
