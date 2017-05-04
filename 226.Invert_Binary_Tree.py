078a830c3504bc2e9e5c31956fbe5a890d9d89c2
lee <wzqnls@gmail.com>
2017-05-04 13:33:16 +0800

    226.Invert Binary Tree

---
+++ 226.Invert_Binary_Tree.py
@@ -0,0 +1,42 @@
+"""
+Invert a binary tree.
+
+     4
+   /   \
+  2     7
+ / \   / \
+1   3 6   9
+
+to
+
+     4
+   /   \
+  7     2
+ / \   / \
+9   6 3   1
+"""
+
+"""
+解题思路：
+递归求解，从根节点开始，每个节点做判断
+若存在则进行子节点的的反转
+"""
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def invertTree(self, root):
+        """
+        :type root: TreeNode
+        :rtype: TreeNode
+        """
+        if root:
+            root.left, root.right = self.invertTree(root.right), self.invertTree(root.left)
+        return root
+            
+            
